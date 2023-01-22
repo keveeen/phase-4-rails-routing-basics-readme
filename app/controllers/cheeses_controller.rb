3a83f5364f84670291a76b18501c36988b12b2bd
keveeen <105822487+keveeen@users.noreply.github.com>
2023-01-22 15:19:12 +0100

    commit tree 12ef7f1dd0d30d13fea9b80d40a02024e35476a1

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,10 @@
+class CheesesController < ApplicationController
+
+    def index
+        def index
+            cheeses = Cheese.all
+            render json: cheeses
+          end
+    end
+
+end
